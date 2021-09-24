4c55d3d8e83176f86a1ab4309dfca227c764ca08
RiasGRMRY <kevinkrajcsi@gmail.com>
2021-09-24 16:39:49 +0200

    a

---
+++ 09.24/Types.py
@@ -0,0 +1,17 @@
+input_data = input("Kérek egy egész szémot: ")
+try:
+    int_number = int(input_data)
+    print("A szám 3-szorosa: ", 3 * int_number)
+    print(type(input_data))
+    print(type(int_number))
+except ValueError:
+    print("Nem egész számot adott meg!")
+
+input_data = input("Kérek egy valós számot: ")
+try:
+    float_number = float(input_data)
+    print("A szám 3-szorosa: ", 3 * float_number)
+    print(type(input_data))
+    print(type(float_number))
+except ValueError:
+    print("Nem valós számot adott meg!")
